d2ded7bbf64d60007481f4221097735e2fe832e7
TZbooo <helloworldbooo@gmail.com>
2023-08-13 08:14:57 +0300

    update .gitignore and add setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 import setuptools
 
 
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
 setuptools.setup(
     name='text_randomizer',
     version='0.0.1',
     author='Timur Zolotov TZbooo netbiom',
+    author_email='helloworldbooo@gmail.com',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     description='Implementation of templating random values inside text',
-    packages=['text_randomizer'],
+    packages=setuptools.find_packages(),
     maintainer='https://github.com/TZbooo',
-    maintainer_email='helloworldbooo@gmail.com'
+    maintainer_email='helloworldbooo@gmail.com',
+    keywords=['text randomizer']
 )
