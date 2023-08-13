78f7fccf9966c7251274af2f35b6a0b7a42c1b42
TZbooo <helloworldbooo@gmail.com>
2023-08-13 06:17:28 +0300

    add README.md and LICENSE

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name='text_randomizer',
+    version='0.0.1',
+    author='Timur Zolotov TZbooo netbiom',
+    description='Implementation of templating random values inside text',
+    packages=['text_randomizer'],
+    maintainer='https://github.com/TZbooo',
+    maintainer_email='helloworldbooo@gmail.com'
+)
