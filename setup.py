9a72379137c7b95fc620974fefca677e3c17e875
TZbooo <helloworldbooo@gmail.com>
2023-08-14 11:17:04 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setuptools.setup(
     name='text_randomizer',
-    version='0.0.2',
+    version='0.0.3',
     author='Timur Zolotov TZbooo netbiom',
     author_email='helloworldbooo@gmail.com',
     long_description=readme(),
